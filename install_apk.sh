11f384dcd22a272aba583789551d311b60135d27
Mocanu Petre Marian <mocepetr@amazon.com>
2024-08-21 15:04:56 +0300

    switch to selfhosted

--- install_apk.sh
+++ install_apk.sh
@@ -8,6 +8,7 @@ $ADB install $APK
 
 echo "Starting screenrecord"
 $ADB emu screenrecord start --time-limit 460 maestro.webm
+export PATH="$PATH":"$HOME/.maestro/bin"
 
 echo "Starting maestro"
 export MAESTRO_DRIVER_STARTUP_TIMEOUT=20000 # setting 20 seconds
