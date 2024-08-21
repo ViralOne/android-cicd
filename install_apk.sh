7277678bb91866c399939a97deb76e02ad2c7a5b
Mocanu Petre Marian <mocepetr@amazon.com>
2024-08-21 15:13:56 +0300

    trigger

--- install_apk.sh
+++ install_apk.sh
@@ -10,6 +10,6 @@ echo "Starting screenrecord"
 $ADB emu screenrecord start --time-limit 460 maestro.webm
 export PATH="$PATH":"$HOME/.maestro/bin"
 
-echo "Starting maestro"
+echo "Starting maestro "
 export MAESTRO_DRIVER_STARTUP_TIMEOUT=20000 # setting 20 seconds
 maestro test -e APP_ID=org.wikipedia --no-ansi flow.yaml
