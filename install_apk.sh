b322e70d0b73d13a3cd88e5197d74ab61f53f703
Mocanu Petre Marian <mocepetr@amazon.com>
2024-08-21 15:20:51 +0300

    fix vm

--- install_apk.sh
+++ install_apk.sh
@@ -6,10 +6,14 @@ APK="./wiki.apk"
 echo "Installing APK"
 $ADB install $APK
 
-echo "Starting screenrecord"
-$ADB emu screenrecord start --time-limit 460 maestro.webm
-export PATH="$PATH":"$HOME/.maestro/bin"
+# echo "Starting screenrecord"
+# $ADB emu screenrecord start --time-limit 460 maestro.webm
+if [ ! -f "/usr/local/bin/maestro" ]; then
+    export PATH="$PATH":"$HOME/.maestro/bin"
+else
+    echo "Maestro already installed"
+fi
 
-echo "Starting maestro "
+echo "Starting maestro"
 export MAESTRO_DRIVER_STARTUP_TIMEOUT=20000 # setting 20 seconds
 maestro test -e APP_ID=org.wikipedia --no-ansi flow.yaml
