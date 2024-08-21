88c8c55bc1bd26036b290698425e06b143dcd916
Mocanu Petre Marian <mocepetr@amazon.com>
2024-08-21 14:24:57 +0300

    wip

---
+++ .github/install_apk.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ADB="/usr/bin/adb"
+APK="./wiki.apk"
+
+echo "Installing APK"
+$ADB install $APK
+
+echo "Starting screenrecord"
+$ADB emu screenrecord start --time-limit 460 maestro.webm
+
+echo "Starting maestro"
+export MAESTRO_DRIVER_STARTUP_TIMEOUT=20000 # setting 20 seconds
+maestro test -e APP_ID=org.wikipedia --no-ansi flow.yaml
