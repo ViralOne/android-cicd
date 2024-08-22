4863a612a12f5fd9d96a0ab88f75b58715b9b260
Mocanu Petre Marian <mocepetr@amazon.com>
2024-08-22 12:57:39 +0300

    update ci script

---
+++ ci-setup.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Define constants for ADB and APK paths
+ADB="/usr/bin/adb"
+APK="./wiki.apk"
+MAESTRO_BIN="$HOME/.maestro/bin"
+TIMEOUT=30000
+
+export PATH="$PATH:$MAESTRO_BIN"
+
+# # List connected ADB devices and filter for the device ID
+# device_id=$(adb devices | awk 'NR==2 {print $1}')
+
+# # Check if a device ID was found
+# if [ -z "$device_id" ]; then
+#     echo "No ADB devices found. Exiting."
+#     exit 1
+# fi
+
+# # Connect to the specified device
+# echo "Connecting to device $device_id..."
+# if adb connect "$device_id"; then
+#     echo "Successfully connected to $device_id."
+# else
+#     echo "Failed to connect to $device_id."
+#     exit 1
+# fi
+
+# Install the APK
+echo "Installing APK..."
+if $ADB install "$APK"; then
+    echo "APK installed successfully."
+else
+    echo "Failed to install APK."
+    exit 1
+fi
+
+# echo "Starting screenrecord"
+# $ADB emu screenrecord start --time-limit 460 maestro.webm
+
+# Start Maestro test
+echo "Starting Maestro..."
+export MAESTRO_DRIVER_STARTUP_TIMEOUT=$TIMEOUT
+if maestro test -e APP_ID=org.wikipedia --format=junit --output=result.xml --no-ansi flow.yaml; then
+    echo "Maestro test completed successfully."
+else
+    echo "Maestro test failed."
+    exit 1
+fi
